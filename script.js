0a7cfb84427009b8a5c09f8c563776338cdcd7df
Juan <jdondayr@gmail.com>
2026-04-10 17:10:56 +0200

    Añado contadores para las tareas totales y las completadas

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let botonAñadirImportante = document.querySelector("#botonAñadirImportante");
 let botonBorrarTodasTareas = document.querySelector("#botonBorrarTodasTareas");
 let listaTareas = document.querySelector("#listaTareas");
 let listaImportantes = document.querySelector("#listaImportantes");
+let tareasTotales = document.querySelector("#tareasTotales");
+let tareasCompletadas = document.querySelector("#tareasCompletadas");
+let contadorTareasTotales = document.querySelector("#contadorTareasTotales");
+let contadorTareasCompletadas = document.querySelector("#contadorTareasCompletadas");
 
 // Eventos
 botonAñadir.addEventListener("click", () => {
@@ -20,15 +24,19 @@ botonAñadir.addEventListener("click", () => {
     // Marcar las tareas como completadas
     tareaNueva.addEventListener("click", () => {
         tareaNueva.classList.toggle("completadas");
+        if (tareaNueva.classList.contains("completadas")) Number(contadorTareasCompletadas.textContent++);
+        else contadorTareasCompletadas.textContent--;
     });
 
     // Borrar la tarea con el botón
     botonBorrarTarea.addEventListener("click", () => {
         listaTareas.removeChild(tareaNueva);
+        Number(contadorTareasTotales.textContent--);
     })
 
     tareaNueva.appendChild(botonBorrarTarea);
     listaTareas.appendChild(tareaNueva);
+    Number(contadorTareasTotales.textContent++);
 });
 
 botonAñadirImportante.addEventListener("click", () => {
@@ -44,20 +52,26 @@ botonAñadirImportante.addEventListener("click", () => {
     // Marcar las tareas como completadas
     tareaNueva.addEventListener("click", () => {
         tareaNueva.classList.toggle("completadas");
+        if (tareaNueva.classList.contains("completadas")) Number(contadorTareasCompletadas.textContent++);
+        else contadorTareasCompletadas.textContent--;
     });
 
     // Borrar la tarea con el botón
     botonBorrarTarea.addEventListener("click", () => {
         listaImportantes.removeChild(tareaNueva);
+        Number(contadorTareasTotales.textContent--);
     })
 
     tareaNueva.appendChild(botonBorrarTarea);
     listaImportantes.appendChild(tareaNueva);
+    Number(contadorTareasTotales.textContent++);
 });
 
 botonBorrarTodasTareas.addEventListener("click", () => {
     let itemsListaTareas = listaTareas.querySelectorAll("li");
     let itemsListaImportantes = listaImportantes.querySelectorAll("li");
     itemsListaTareas.forEach(item => item.remove());
-    itemsListaImportantes.forEach(item => item.remove()); 
+    itemsListaImportantes.forEach(item => item.remove());
+    Number(contadorTareasTotales.textContent = 0);
+    Number(contadorTareasCompletadas.textContent = 0);
 })
